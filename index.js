45478e32c9b3f1226d5856442b55ec7a88f2a59a
ankur4591 <ankur.verma@repolite.com>
2025-03-20 21:53:02 +0530

    added default check

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use(
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello from proxy server!");
+});
+
 // Proxy endpoint for Places API text search
 app.get("/api/places/textsearch", async (req, res) => {
   try {
